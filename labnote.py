a1f9d3fa467a20bd2062770ed91184f9add148b1
Thomas Krug <t.krug@elektronenpumpe.de>
2017-04-25 19:35:22 +0200

    correctly handle html load base on live update

--- labnote.py
+++ labnote.py
@@ -787,8 +787,7 @@ class mainwindow():
 
         html = self.render(rst, lock=True)
 
-        base = os.path.basename(self.current_file) + "/" + os.path.dirname(self.current_file)
-        base = "file://" + base
+        base = "file://dummy.rst/" + os.path.dirname(self.current_file) + "/" + os.path.basename(self.current_file)
         log.debug("base " + base)
         self.ignore_modified = True
         self.webview.load_html(html, base)
