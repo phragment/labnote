8b94949732de4a14c31c027da2c5969440fec95b
Thomas Krug <t.krug@elektronenpumpe.de>
2019-03-16 03:46:54 +0100

    use install

--- install.sh
+++ install.sh
@@ -1,13 +1,11 @@
 #!/bin/sh
 
-sudo cp labnote.py /usr/local/bin/labnote
-sudo chmod +x /usr/local/bin/labnote
-
+sudo install -Dm755 labnote.py /usr/local/bin/labnote
 sudo -k
 
 mkdir -p ~/.config/labnote
 cp config/* ~/.config/labnote/
 
-cp labnote.desktop ~/.local/share/applications/
+install -Dm644 labnote.desktop ~/.local/share/applications/labnote.desktop
 update-desktop-database ~/.local/share/applications/
 
